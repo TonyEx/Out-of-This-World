eef2e7777f36dc166ad2ada99b283535736e0b7a
Tony Angelo <tonyangelox@veronica.home>
2014-06-19 21:22:08 -0400

    First commit.  Lessons through 106.

---
+++ OWOuterSpaceTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  OWOuterSpaceTableViewController.h
+//  Out of this World
+//
+//  Created by Tony Angelo on 6/19/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWOuterSpaceTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *planets;
+
+@end
