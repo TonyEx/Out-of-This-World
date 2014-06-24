cc1b32f31ad23ba2d571717bef93788f90b8d8f5
Tony Angelo <tonyangelox@aristoi.home>
2014-06-24 19:41:15 -0400

    Completed through lesson 128.

---
+++ Out of this World/OWSpaceImageViewController.h
@@ -0,0 +1,17 @@
+//
+//  OWSpaceImageViewController.h
+//  Out of this World
+//
+//  Created by Tony Angelo on 6/24/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWSpaceImageViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIScrollView *ScrollView;
+
+@property(strong, nonatomic) UIImageView *imageView;
+
+@end
---
+++ Out of this World/OWSpaceImageViewController.m
@@ -0,0 +1,54 @@
+//
+//  OWSpaceImageViewController.m
+//  Out of this World
+//
+//  Created by Tony Angelo on 6/24/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import "OWSpaceImageViewController.h"
+
+@interface OWSpaceImageViewController ()
+
+@end
+
+@implementation OWSpaceImageViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"Jupiter.jpg"]];
+    self.ScrollView.contentSize = self.imageView.frame.size;
+    [self.ScrollView addSubview:self.imageView];
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
